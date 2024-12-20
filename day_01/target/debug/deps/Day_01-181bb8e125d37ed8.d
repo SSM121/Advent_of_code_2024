c3e89563cf97cabefa29e126b1c404a3f4737bac
Spencer Kasteler <sdkasteler@gmail.com>
2024-12-20 12:12:07 -0700

    rust wants snake case folder name. init a cargo project for day 1

---
+++ day_01/target/debug/deps/Day_01-181bb8e125d37ed8.d
@@ -0,0 +1,5 @@
+/home/sdkasteler/projects/Advent_of_code_2024/Day_01/target/debug/deps/Day_01-181bb8e125d37ed8: src/main.rs
+
+/home/sdkasteler/projects/Advent_of_code_2024/Day_01/target/debug/deps/Day_01-181bb8e125d37ed8.d: src/main.rs
+
+src/main.rs:
