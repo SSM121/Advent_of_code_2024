892bb18824107e14904df0f329b3a34d2d3006f8
Spencer Kasteler <sdkasteler@gmail.com>
2024-12-20 13:16:29 -0700

    pulled input file io into its own rust library adeventio for reuse in second day

---
+++ adventio/src/lib.rs
@@ -0,0 +1,16 @@
+use std::fs;
+
+pub fn read_inputs(filename: String) -> (Vec<isize>, Vec<isize>){
+    let data = fs::read_to_string(filename).expect("Unable to read file");
+    let lines = data.lines();
+    let mut list1: Vec<isize> = Vec::new();
+    let mut list2: Vec<isize> = Vec::new();
+    for line in lines{
+        let mut words = line.split_whitespace();
+        let x = words.next().unwrap().parse::<isize>().unwrap();
+        list1.push(x); 
+        let y = words.next().unwrap().parse::<isize>().unwrap();
+        list2.push(y);   
+    }
+    (list1, list2)
+}
--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -1,27 +1,11 @@
-use std::fs;
-
+use adventio;
 fn main() {
-    let (mut list1, mut list2) = read_inputs("input.txt".to_string());
+    let (mut list1, mut list2) = adventio::read_inputs("input.txt".to_string());
     list1.sort();
     list2.sort();
     let distances = get_distances(list1, list2);
     let sum: isize = distances.iter().sum();
     println!("Sum = {sum}");
-}
-
-fn read_inputs(filename: String) -> (Vec<isize>, Vec<isize>){
-    let data = fs::read_to_string(filename).expect("Unable to read file");
-    let lines = data.lines();
-    let mut list1: Vec<isize> = Vec::new();
-    let mut list2: Vec<isize> = Vec::new();
-    for line in lines{
-        let mut words = line.split_whitespace();
-        let x = words.next().unwrap().parse::<isize>().unwrap();
-        list1.push(x); 
-        let y = words.next().unwrap().parse::<isize>().unwrap();
-        list2.push(y);   
-    }
-    (list1, list2)
 }
 
 
