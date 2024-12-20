df9cd4c66b67b483c36fa3830f24a17d4275a2a2
Spencer Kasteler <sdkasteler@gmail.com>
2024-12-20 14:17:30 -0700

    setup and tested input for day 02

--- adventio/src/lib.rs
+++ adventio/src/lib.rs
@@ -15,6 +15,21 @@ pub fn read_inputs_day_01(filename: String) -> (Vec<isize>, Vec<isize>){
     (list1, list2)
 }
 
+pub fn read_inputs_day_02(filename: String)-> Vec<Vec<isize>>{
+    let mut out: Vec<Vec<isize>> = Vec::new();
+    let data = fs::read_to_string(filename).expect("Unable to read file");
+    let lines = data.lines();
+    for line in lines{
+        let words = line.split_whitespace();
+        let mut l: Vec<isize> = Vec::new();
+        for word in words{
+            l.push(word.parse::<isize>().unwrap());
+        }
+        out.push(l);
+    }
+    out
+}
+
 #[cfg(test)]
 mod tests{
     use super::*;
@@ -25,5 +40,16 @@ mod tests{
         let goal2 = vec![4, 3, 5, 3, 9, 3,];
         assert_eq!(goal1, list1);
         assert_eq!(goal2, list2);
+    }
+    #[test]
+    fn day02(){
+        let out = read_inputs_day_02("test2.txt".to_string());
+        let goal = vec![    vec![7, 6, 4, 2, 1,],
+                            vec![1, 2, 7, 8, 9,],
+                            vec![9, 7, 6, 2, 1,],
+                            vec![1, 3, 2, 4, 5,],
+                            vec![8, 6, 4, 4, 1,],
+                            vec![1, 3, 6, 7, 9,],];
+        assert_eq!(out, goal);
     }
 }
