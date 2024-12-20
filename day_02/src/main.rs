50782e55b12cac0dc52bbea70ed4bed54cc78161
Spencer Kasteler <sdkasteler@gmail.com>
2024-12-20 13:39:05 -0700

    solved day 2. also added the test from the website as a comment to prove the code worked initially. could come back and add the test

--- day_02/src/main.rs
+++ day_02/src/main.rs
@@ -1,3 +1,27 @@
+use adventio;
+
 fn main() {
-    println!("Hello, world!");
+    let (list1, list2) = adventio::read_inputs("../input.txt".to_string());
+//    let (list1, list2) = adventio::read_inputs("test.txt".to_string());
+    let sims = get_similarities(list1, list2);
+    let sum: isize  = sims.iter().sum();
+    println!("{sum}");
 }
+
+fn get_similarities(list1: Vec<isize>, list2: Vec<isize>)-> Vec<isize>{
+    let mut similarities: Vec<isize> = Vec::new();
+    if list1.len() != list2.len(){
+        panic!("Lists are different lengths");
+    }
+    for num in list1{
+        let mut sims: isize = 0;
+        for n in &list2{
+            if *n == num{
+            sims +=1;
+            }
+        }
+        similarities.push(num * sims);
+    }
+    similarities
+}
+
